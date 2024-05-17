b24e6e04db06b3677498dd56f1acb6c84562a83f
angelofaoldi <faoldiangelo@gmail.com>
2024-05-17 08:57:54 +0200

    [ADD] img slideshow

--- js/carosello.js
+++ js/carosello.js
@@ -10,16 +10,62 @@ const images = [
     {image: 'img/05.webp', title: "Marvel's Avengers", text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',}
 ];
 
-console.log(images);
+// Ref HTML
+const items = document.querySelector('.items');
+const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
+
+// tiene traccia della slide attiva
+// e imposta la slide di default
+let activeSlide = 0;
+
+// console.log(images[0]);
+// console.log(images[1]);
+// console.log(images[2]);
+
+// loop ci permette di iterare un array da inizio a fine
+for(let i = 0; i < images.length; i++) {
+    console.log(images[i]);
+    const slide = images[i];
+
+    // output dinamico HTML delle singole slide
+    items.innerHTML +=  `<div class="item">
+        <img src="${slide.image}" alt="${slide.title}">
+        <h3>${slide.title}</h3>
+        <p>${slide.text}</p>
+    </div>`;
+}
+
+const slides = document.querySelectorAll('.item');
+slides[activeSlide].classList.add('active');
+
+const next = document.querySelector(".next");
+next.addEventListener("click", function() {
+    console.log("Ho cliccato su next");
+
+    // increment activeSlide
+    
+    if(activeSlide == images.length -1) {
+        activeSlide = 0
+    } else {
+        activeSlide += 1;
+    }
+    
+    // reset old active slide
+    document.querySelector('.item.active').classList.remove('active')
+
+    // set new active slide
+    slides[activeSlide].classList.add('active');
+}
+);
+
 
 /* creo costanti per ogni elemento html da gestire successivamente. gli elementi li seleziono con il querySelector in modo da non andare a modificare html perché non sempre in futuro sarà possibile averne accesso */
 
-const items = document.querySelector('.items');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const div = document.querySelector('.item');
 
-console.log(items); console.log(prev); console.log(next); console.log(document);
+// const div = document.querySelector('.item');
+
+// console.log(items); console.log(prev); console.log(next); console.log(document);
 
 // utilizzo il metodo degli array: forEach per ciclare attraverso l'array images: images.forEach()
 
